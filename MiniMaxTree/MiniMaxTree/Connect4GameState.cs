167208d3d5bb17a6429bdf7d30616c491cb3a8d7
Lorenzo Lopez <geltbm@gmail.com>
2020-07-06 17:21:54 -0700

    A bit of code for the MinMax tree

---
+++ MiniMaxTree/MiniMaxTree/Connect4GameState.cs
@@ -0,0 +1,42 @@
+﻿using NeuralNetworkLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniMaxTree
+{
+    enum Connect4Color
+    {
+        None,
+        Red,
+        Yellow
+    }
+
+    class Connect4GameState : IMinMaxGameState
+    {
+        public int Value { get; set; }
+        public List<IMinMaxGameState> Children { get; set;}
+
+        private Connect4Color[,] gameState;
+        
+        public Connect4GameState(Connect4Color[,] gameState)
+        {
+            this.gameState = gameState;
+        }
+        public void GenerateChildren()
+        {
+            for(int x = 0; x < gameState.GetLength(0); x++)
+            {
+                for(int y = 0; y < gameState.GetLength(1); y++)
+                {
+                    Connect4Color color = gameState[x, y];
+                    if(color == Connect4Color.None)
+                    {
+                        Connect4Color[,] newGameState = new Connect4Color[gameState.GetLength(0), gameState.GetLength(1)];
+                        gameState.CopyTo(newGameState, 0);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ NeuralNetwork/NeuralNetworkLibrary/MinMaxTree.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuralNetworkLibrary
+{
+    public interface IMinMaxGameState
+    {
+        int Value { get; set; }
+        List<IMinMaxGameState> Children { get; set; }
+        void GenerateChildren();
+    }
+
+    public static class MinMaxFuncs
+    {
+        public static void GenerateTree(IMinMaxGameState startState)
+        {
+            Queue<IMinMaxGameState> states = new Queue<IMinMaxGameState>();
+            states.Enqueue(startState);
+            while(states.Count > 0)
+            {
+                IMinMaxGameState state = states.Dequeue();
+                state.GenerateChildren();
+                foreach(IMinMaxGameState child in state.Children)
+                {
+                    states.Enqueue(child);
+                }
+            }
+        }
+        public static int MinMax(bool isMax, IMinMaxGameState gameState)
+        {
+            if(gameState.Children.Count == 0)
+            {
+                return gameState.Value;
+            }
+
+            if(isMax)
+            {
+                int maxVal = int.MinValue;
+                foreach (IMinMaxGameState child in gameState.Children)
+                {
+                    int childVal = MinMax(false, child);
+                    maxVal = maxVal > childVal ? maxVal : childVal;
+                }
+                return maxVal;
+            }
+            else
+            {
+                int minVal = int.MaxValue;
+                foreach (IMinMaxGameState child in gameState.Children)
+                {
+                    int childVal = MinMax(true, child);
+                    minVal = minVal < childVal ? minVal : childVal;
+                }
+                return minVal;
+            }
+        }
+    }
+}
